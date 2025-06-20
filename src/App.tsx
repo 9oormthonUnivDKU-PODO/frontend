3e20a953f12778ba54407e67fb4286fda8060a80
seyun31 <2ne1jenna@naver.com>
2025-06-20 23:01:48 +0900

    feat: 메인 홈 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import ListProfessor from "./pages/ListProfessor";
 import LiveProfessor from "./pages/LiveProfessor";
+import MainLogin from "./pages/MainLogin";
 
 function App() {
 	return (
 		<>
 			<Routes>
+				<Route path="/" element={<MainLogin />} />
 				<Route path="/listp" element={<ListProfessor />} />
 				<Route path="/livep" element={<LiveProfessor />} />
 			</Routes>
